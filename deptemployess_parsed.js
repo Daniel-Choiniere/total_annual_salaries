6ab2215185fe871668d2ffb559a08fa16f05dd79
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-15 17:09:33 +0000

    parsed through each file to create workable arrays

---
+++ deptemployess_parsed.js
@@ -0,0 +1,18 @@
+// fetches built in Node method 'fs'
+var fs = require('fs');
+
+var input = fs.createReadStream('load_dept_emp.txt');
+
+// create the readline interface
+var rl = require('readline').createInterface({
+   input: input,
+   terminal: false
+});
+
+// parse through the data line by line getting rid of unnecessary characters
+// split the data into a workable array at each comma
+// splice off the last blank value in the array
+rl.on('line', function(line) {
+    line = line.replace(/'|\(|\)/g,'').split(',').splice(0,4);
+    console.log(line);
+});
---
+++ employess_parsed.js
@@ -0,0 +1,20 @@
+// fetches built in Node method 'fs'
+var fs = require('fs');
+
+var input = fs.createReadStream('load_employess.txt');
+
+// create the readline interface
+var rl = require('readline').createInterface({
+   input: input,
+   terminal: false
+});
+
+// parse through the data line by line getting rid of unnecessary characters
+// split the data into a workable array at each comma
+// splice off the last blank value in the array
+rl.on('line', function(line) {
+    line = line.replace(/'|\(|\)/g,'').split(',').splice(0,6);
+    console.log(line);
+});
+
+
