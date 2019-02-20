cd854635c8b45ae7c7067bb671d12a03e6b2e82e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-02-20 01:10:47 +0000

    calculated the total salary for all departments, and the total salary for each individual department

--- arnellsExample.js
+++ arnellsExample.js
@@ -5,11 +5,28 @@ var employeeId = [ [100, 102, 103, 104, 105], [200, 201, 202, 203, 204, 205], [3
 var salaries = [ [85000, 85000, 95000, 105000, 150000], [65000, 65000, 75000, 75000, 85000, 95000], [55000, 65000, 75000, 95000], [55000, 65000, 75000] ];
 var employeeName = [ ["Ruth", "Sofía", "Elias", "Mathias", "Arnell"], ["Kai", "Isaiah", "Amare", "Nadia", "Nolan", "Jason"], ["Alejandro", "Steven", "Brodi", "Mary"], ["Jose", "Nia", "Lee"] ];
 
+
+
+let totalSalary = 0;
+
+
 console.log(business);
+
+
 for (var i = 0; i < departmentId.length; i++) {
     console.log(`Department ${departmentId[i]} - ${departments[i]}:`);
     
+   let eachDept = 0;
+    
     for (var j=0; j < salaries[i].length; j++) {
+        
         console.log(` ${j+1}: Employee ID: ${employeeId[i][j]}, Name: ${employeeName[i][j]}, Salary: ${salaries[i][j]}` );
+    
+        eachDept += salaries[i][j];
+        totalSalary += salaries[i][j];
+    
     }
+    console.log(`The total salary for ${departments[i]} is: ${eachDept}`);
 }
+console.log('\n')
+console.log("Total salary " + totalSalary);
